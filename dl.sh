76e2c2bca0c88d9e361a1f545eb0c099f2668399
Frank Keenan <frank.keenan@oup.com>
2023-09-11 17:32:08 +0100

    Updated

--- dl.sh
+++ dl.sh
@@ -1,3 +1,6 @@
-curl -s --user frank.keenan:wltncrs40 "https://dws-dps.idm.fr/api/v1/projects/EN_MERGEDDICT_A2361_00001/entries/export/allInternalAttributesAndAdditionalMetadata"  | perl lose_suppressed.pl > /data_c/Projects/OL/EN_MERGEDDICT_A2361_00001/dps.xml 
-cat /data_c/Projects/OL/EN_MERGEDDICT_A2361_00001/dps.xml | perl get_ode.pl > /data_c/Projects/OL/EN_MERGEDDICT_A2361_00001/dps_ode.xml
-cat /data_c/Projects/OL/EN_MERGEDDICT_A2361_00001/dps.xml | perl get_noad.pl > /data_c/Projects/OL/EN_MERGEDDICT_A2361_00001/dps_noad.xml
+curl -s --user frank.keenan:wltncrs40 "https://dws-dps.idm.fr/api/v1/projects/EN_MERGEDDICT_A2361_00001/entries/export/allInternalAttributesAndAdditionalMetadata"  | perl oneline.pl | perl lose_suppressed.pl > /data/data_c/Projects/OL/EN_MERGEDDICT_A2361_00001/dps.xml 
+cat /data/data_c/Projects/OL/EN_MERGEDDICT_A2361_00001/dps.xml | perl get_ode.pl > /data/data_c/Projects/OL/EN_MERGEDDICT_A2361_00001/dps_ode.xml
+cat /data/data_c/Projects/OL/EN_MERGEDDICT_A2361_00001/dps.xml | perl get_noad.pl > /data/data_c/Projects/OL/EN_MERGEDDICT_A2361_00001/dps_noad.xml
+perl check_internal_xrefs.pl -r "NOAD_fail_xrefs.xlsx" dps_noad.xml
+perl check_internal_xrefs.pl -r "ODE_fail_xrefs.xlsx" dps_ode.xml
+
